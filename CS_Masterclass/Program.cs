9deb4179ca9b2412b779ad0b1b0548bae33bf535
gborie <gborie7@gmail.com>
2018-07-03 12:08:46 -0400

    String data type

--- CS_Masterclass/Program.cs
+++ CS_Masterclass/Program.cs
@@ -10,18 +10,9 @@ namespace CS_Masterclass
     {
         static void Main(string[] args)
         {
-            int num1 = 13;
-            int num2 = 35;
-            int sum = num1 + num2;
+            string myname = "Guillaume";
 
-            double d1 = 3.5;
-            double d2 = 1.337;
-            double sumD = d1 + d2;
-
-            float f1 = 3.5F;
-
-            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum );
-            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
+            Console.WriteLine("My name is " + myname);
             Console.Read();
         }
         
