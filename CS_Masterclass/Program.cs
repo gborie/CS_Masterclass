256df1cccf67536a59fecf4c6b447272fd0f045f
gborie <gborie7@gmail.com>
2018-07-03 09:23:28 -0400

    Print Variable num1

--- CS_Masterclass/Program.cs
+++ CS_Masterclass/Program.cs
@@ -8,22 +8,13 @@ namespace CS_Masterclass
 {
     class Program
     {
-        // Starting point of program
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.Clear();
-            Console.WriteLine("Hello GB");
-            Console.WriteLine("C# is cool!");
+            int num1 = 13;
+            Console.WriteLine(num1);
             Console.Read();
 
         }
         
-        /* This program
-         * is made in order to
-         * learn how to print text
-         * in console
-         */
     }
 }
