498bfddab43616f7d14174610dde7acd2457245e
gborie <gborie7@gmail.com>
2018-07-03 09:05:36 -0400

    Changed color console

--- CS_Masterclass/Program.cs
+++ CS_Masterclass/Program.cs
@@ -11,9 +11,13 @@ namespace CS_Masterclass
         // Starting point of program
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
             Console.WriteLine("Hello GB");
             Console.WriteLine("C# is cool!");
             Console.Read();
+
         }
         
         /* This program
