33ca3750f307022017998c600516750854779956
gborie <gborie7@gmail.com>
2018-07-03 12:04:08 -0400

    Add double type

--- CS_Masterclass/Program.cs
+++ CS_Masterclass/Program.cs
@@ -14,7 +14,13 @@ namespace CS_Masterclass
             int num2 = 35;
             int sum = num1 + num2;
 
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 + d2;
+
+
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum );
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
             Console.Read();
         }
         
